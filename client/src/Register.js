e1846ccbdada545670653312a6bbf91d62dd1b09
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-06-01 00:46:02 -0600

    MODIFY
    Add updating while we type

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
+
 function Register () {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
     return(
         <form action="">
-            <input type="email" placeholder="email"/><br/>
-            <input type="password" placeholder="password"/><br/>
+            <input type="email" placeholder="email" value={email} onChange={e => setEmail(e.target.value)}/><br/>
+            <input type="password" placeholder="password" value={password} onChange={e => setPassword(e.target.value)}/><br/>
             <button type="submit">register</button>
         </form>
     );
