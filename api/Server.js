f149f09aec2fe0362bfcef1ad5eaea7aea0c5823
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-06-02 00:15:41 -0600

    MODIFY
    - Adding database connection
    - Using .env file for security enhance

--- api/Server.js
+++ api/Server.js
@@ -1,10 +1,25 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
 
 const app = express();
+app.use(cookieParser());
+app.use(bodyParser.json({extended:true}));
+dotenv.config();
+
+// DB Connection
+await mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology:true});
 
 // API Requests
 app.get('/', (req,res) => {
     res.send('ok');
+})
+
+app.post('/register', (req,res) => {
+    const {email,password} = req.body;
 })
 
 app.listen(4000);
